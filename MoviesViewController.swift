df2ad30d597b2e472ea4371ba7932e9cc30cde1d
ljenn <ljenn@users.noreply.github.com>
2021-04-11 20:36:32 -0700

    Tabs & grid view

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    
     
 
     @IBOutlet weak var tableView: UITableView!
@@ -71,14 +69,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        // find selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        // pass selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
